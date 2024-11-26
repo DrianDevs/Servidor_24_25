05a9c1bc54f5afb5f0447d3c2306f8e587a8085b
DrianDevs <adriangm8110@gmail.com>
2024-11-26 13:04:28 +0100

    Proyecto VideoClub Avanzado

---
+++ Ejercicios/Proyecto_Videoclub/app/Soporte.php
@@ -0,0 +1,43 @@
+<?php
+class Soporte
+{
+    public $titulo;
+    protected $numero;
+    private $precio;
+    private const IVA = 0.21;
+
+    public function __construct($titulo, $numero, $precio)
+    {
+        $this->titulo = $titulo;
+        $this->numero = $numero;
+        $this->precio = $precio;
+
+    }
+
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    public function getPrecioConIVA()
+    {
+        return $this->precio * (1 + self::IVA);
+    }
+
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    public function muestraResumen()
+    {
+        echo "<br>";
+        echo $this->titulo;
+        echo "<br>";
+        echo $this->precio . " € (IVA no incluido)";
+
+    }
+}
+
+
+?>
