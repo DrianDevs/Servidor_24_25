c96121e2912a58c161cbc25e743bb1199de39b37
DrianDevs <adriangm8110@gmail.com>
2024-11-27 21:52:28 +0100

    Proyecto VideoClub Terminado

---
+++ Ejercicios/Proyecto_Videoclub/app/Juego.php
@@ -0,0 +1,36 @@
+<?php
+class Juego extends Soporte
+{
+    public $consola;
+    private $minNumJugadores;
+    private $maxNumJugadores;
+
+    public function __construct($titulo, $numero, $precio, $consola, $minNumJugadores, $maxNumJugadores)
+    {
+        parent::__construct($titulo, $numero, $precio);
+        $this->consola = $consola;
+        $this->minNumJugadores = $minNumJugadores;
+        $this->maxNumJugadores = $maxNumJugadores;
+    }
+
+    public function muestraJugadoresPosibles()
+    {
+        if ($this->minNumJugadores === 1 && $this->maxNumJugadores === 1) {
+            echo "Para un jugador.";
+        } elseif ($this->minNumJugadores === $this->maxNumJugadores) {
+            echo "Para {$this->minNumJugadores} jugadores.";
+        } else {
+            echo "De {$this->minNumJugadores} a {$this->maxNumJugadores} jugadores.";
+        }
+    }
+
+    public function muestraResumen()
+    {
+        echo "<br>Juego para: " . $this->consola;
+        parent::muestraResumen();
+        echo "<br>";
+        $this->muestraJugadoresPosibles();
+    }
+}
+
+?>
