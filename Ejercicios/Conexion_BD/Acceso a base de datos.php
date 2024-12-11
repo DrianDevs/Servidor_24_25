689c1022b58b5399756787c5f4aa2699e3658b35
DrianDevs <adriangm8110@gmail.com>
2024-12-11 13:27:14 +0100

    Conexion BD Avanzado

--- Ejercicios/Conexion_BD/Acceso a base de datos.php
+++ Ejercicios/Conexion_BD/Acceso a base de datos.php
@@ -1,12 +1,30 @@
 <?php
 try {
+    // Configuración de conexión
     $mysql = "mysql:host=localhost;dbname=dwes_manana_prueba;charset=UTF8";
-    $user = "root";
-    $password = "";
-    $conexion = new PDO($mysql, $user, $password);
-    echo "<p>Conectada a la BBDD</p>";
+    $user = "dwes_manana";
+    $password = "12345";
+    $opciones = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+    // Conexión a la base de datos
+    $conexion = new PDO($mysql, $user, $password, $opciones);
+    $resultado = $conexion->query('select * FROM mensajes2');
+    echo "<h3>Registros en la tabla 'mensajes2':</h3>";
+    echo "<table border='1'>";
+    echo "<tr><th>ID</th><th>Mensaje</th></tr>";
+
+    foreach ($resultado as $fila) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($fila['id']) . "</td>";
+        echo "<td>" . htmlspecialchars($fila['mensaje']) . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+    $conexion = null;
 } catch (PDOException $e) {
-    // Mostramos el mensaje en caso de error
+    // Mostramos mensaje en caso de error
     echo "<p>" . $e->getMessage() . "</p>";
 }
 ?>
